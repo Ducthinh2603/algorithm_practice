6bd79efa193e90014bdc7d083517c4d89a578a46
thinhduong-gft <thinh.ducduong@gft.com>
2022-11-23 12:16:10 +0700

    solve edit lcs2

--- Course 1/toolbox2208172208/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/LCS2.java
+++ Course 1/toolbox2208172208/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/LCS2.java
@@ -2,9 +2,43 @@ import java.util.*;
 
 public class LCS2 {
 
+    private static void printRs(int[][] rs, int[] s, int[] t) {
+        int n = s.length, m = t.length;
+        System.out.print("  ");
+        for (int k : t) {
+            System.out.print(k + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            System.out.print(s[i] + " ");
+            for (int j = 0; j < m; j++) {
+                System.out.print(rs[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     private static int lcs2(int[] a, int[] b) {
         //Write your code here
-        return Math.min(a.length, b.length);
+        int n = a.length, m = b.length;
+        int[][] rs = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                boolean check = a[i] == b[j];
+                if (i == 0 && j == 0) {
+                    rs[i][j] = check ? 1 : 0;
+                } else {
+                    int temp = 0;
+                    if (i >= 1 && j >= 1) temp = Math.max(temp, rs[i - 1][j - 1]);
+                    if (i >= 1) temp = Math.max(temp, rs[i - 1][j]);
+                    if (j >= 1) temp = Math.max(temp, rs[i][j - 1]);
+                    rs[i][j] = check ? temp + 1 : temp;
+                }
+            }
+        }
+//        printRs(rs, a, b);
+        return rs[n - 1][m - 1];
     }
 
     public static void main(String[] args) {
@@ -20,6 +54,8 @@ public class LCS2 {
         for (int i = 0; i < m; i++) {
             b[i] = scanner.nextInt();
         }
+//        int[] a = new int[] {2, 7, 5};
+//        int[] b = new int[] {2, 5};
 
         System.out.println(lcs2(a, b));
     }
