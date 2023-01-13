cf8e4828c3583c8c986ed087d4d98a32efc545f0
thinhduong-gft <thinh.ducduong@gft.com>
2023-01-13 15:34:30 +0700

    solve find acycling graph

--- Course 3/course3_2020_07_27/week2_graph_decomposition2/1_cs_curriculum/acyclicity.cpp
+++ Course 3/course3_2020_07_27/week2_graph_decomposition2/1_cs_curriculum/acyclicity.cpp
@@ -4,8 +4,49 @@
 using std::vector;
 using std::pair;
 
+int get_unvisited(bool* visited, int n) {
+  for (int i = 0; i < n; i++) {
+    if (!visited[i]) return i;
+  }
+  return -1;
+}
+
+void explore(int ind, vector<vector<int> >& adj, bool* removed, bool* visited) {
+  if (visited[ind] || removed[ind]) return;
+  visited[ind] = true;
+  int m = adj[ind].size();
+  if (m == 0) {
+    removed[ind] = true;
+    return;
+  }
+  else {
+    bool should_removed = true;
+    for (int i = 0; i < m; i++) {
+      explore(adj[ind][i], adj, removed, visited);
+      should_removed &= removed[adj[ind][i]];
+    }
+    if (should_removed) removed[ind] = true;
+    return;
+  }
+}
+
 int acyclic(vector<vector<int> > &adj) {
   //write your code here
+  int n = adj.size();
+  bool* removed = new bool[n];
+  bool* visited = new bool[n];
+  for (int i = 0; i < n; i++) {
+    removed[i] = false;
+    visited[i] = false;
+  }
+  int ind = get_unvisited(visited, n);
+  while (ind != -1) {
+    explore(ind, adj, removed, visited);
+    ind = get_unvisited(visited, n);
+  }
+  for (int i = 0; i < n; i++) {
+    if (!removed[i]) return 1;
+  }
   return 0;
 }
 
