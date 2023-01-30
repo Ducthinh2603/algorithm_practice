1ea18523c39c8b82c370fd2a48427f9659d68334
thinhduong-gft <thinh.ducduong@gft.com>
2023-01-30 14:56:56 +0700

    solve dijkstra

--- Course 3/course3_2020_07_27/week4_paths_in_graphs2/1_minimum_flight_cost/dijkstra.cpp
+++ Course 3/course3_2020_07_27/week4_paths_in_graphs2/1_minimum_flight_cost/dijkstra.cpp
@@ -7,8 +7,49 @@ using std::queue;
 using std::pair;
 using std::priority_queue;
 
+typedef pair<int, long long> map;
+struct comparator {
+  bool operator() (
+    map const& a,
+    map const& b)
+    const
+  {
+    return a.second > b.second;
+  }
+};
+
 long long distance(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, int t) {
   //write your code her
+  int n = adj.size();
+  priority_queue<map, vector<map>, comparator > pq;
+  long long* dist = new long long[n];
+  bool* visited = new bool[n];
+  for (int i = 0; i < n; i++) {
+    dist[i] = -1;
+    visited[i] = false;
+  }
+  dist[s] = 0;
+  pq.push(std::make_pair(s, 0));
+  while(!pq.empty()) {
+    map m = pq.top();
+    pq.pop();
+    int ind = m.first;
+    long long d = m.second;
+    if (visited[ind]) continue;
+
+    if (ind == t) return d;
+    else visited[ind] = true;
+    for(int i = 0; i < adj[ind].size(); i++) {
+      int temp_ind = adj[ind][i];
+      long long c = cost[ind][i];
+      if (!visited[temp_ind]) {
+        if (dist[temp_ind] == -1 || dist[temp_ind] > d + c) {
+          dist[temp_ind] = d + c;
+          pq.push(std::make_pair(temp_ind, dist[temp_ind]));
+        }
+      }
+    }
+  }
   return -1;
 }
 
