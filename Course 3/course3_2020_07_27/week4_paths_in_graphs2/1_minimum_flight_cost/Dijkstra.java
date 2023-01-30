5095d64ade76f74c9bed39267c3f2e325b4ac158
thinhduong-gft <thinh.ducduong@gft.com>
2023-01-30 19:44:59 +0700

    add util/pair.java

--- Course 3/course3_2020_07_27/week4_paths_in_graphs2/1_minimum_flight_cost/Dijkstra.java
+++ Course 3/course3_2020_07_27/week4_paths_in_graphs2/1_minimum_flight_cost/Dijkstra.java
@@ -1,7 +1,53 @@
 import java.util.*;
 
 public class Dijkstra {
+    static class Pair<T, R> {
+        T first;
+        R second;
+        public Pair(T x, R y) {
+            first = x;
+            second = y;
+        }
+    }
+    static Comparator<Pair<Integer, Long>> comparator = new Comparator<Pair<Integer,Long>>() {
+        @Override
+        public int compare(Pair<Integer, Long> a, Pair<Integer, Long> b){
+            if (a.second > b.second) return 1;
+            else if (a.second < b.second) return -1;
+            else return 0;
+        }
+    };
     private static long distance(ArrayList<Integer>[] adj, ArrayList<Integer>[] cost, int s, int t) {
+        int n = adj.length;
+        
+        PriorityQueue<Pair<Integer, Long>> pQueue = new PriorityQueue<>(1, comparator);
+        long[] dist = new long[n];
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            dist[i] = -1;
+            visited[i] = false;
+        }
+        dist[s] = 0;
+        pQueue.add(new Pair<Integer,Long>(s, dist[s]));
+        while(!pQueue.isEmpty()) {
+            Pair<Integer, Long> e = pQueue.poll();
+            int ind = e.first;
+            long d = e.second;
+            if (visited[ind]) continue;
+
+            if (ind == t) return dist[ind];
+            else visited[ind] = true;
+            for (int i = 0; i < adj[ind].size(); i++) {
+                int temp_ind = adj[ind].get(i);
+                long c = cost[ind].get(i);
+                if (!visited[temp_ind]) {
+                    if (dist[temp_ind] == -1 || dist[temp_ind] > d + c) {
+                        dist[temp_ind] = d + c;
+                        pQueue.add(new Pair<Integer,Long>(temp_ind, dist[temp_ind]));
+                    }
+                } 
+            }
+        }
         return -1;
     }
 
@@ -9,8 +55,8 @@ public class Dijkstra {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-        ArrayList<Integer>[] cost = (ArrayList<Integer>[])new ArrayList[n];
+        ArrayList<Integer>[] adj = new ArrayList[n];
+        ArrayList<Integer>[] cost = new ArrayList[n];
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<Integer>();
             cost[i] = new ArrayList<Integer>();
@@ -26,6 +72,7 @@ public class Dijkstra {
         int x = scanner.nextInt() - 1;
         int y = scanner.nextInt() - 1;
         System.out.println(distance(adj, cost, x, y));
+        scanner.close();
     }
 }
 
---
+++ utils/java/Pair.java
@@ -0,0 +1,23 @@
+package utils.java;
+import java.util.*;
+
+public class Pair<T, R> {
+    T first;
+    R second;
+
+    public Pair(T x, R y) {
+        first = x;
+        second = y;
+    }
+
+
+    // Example
+    static Comparator<Pair<Integer, Long>> comparator = new Comparator<Pair<Integer,Long>>() {
+        @Override
+        public int compare(Pair<Integer, Long> a, Pair<Integer, Long> b){
+            if (a.second > b.second) return 1;
+            else if (a.second < b.second) return -1;
+            else return 0;
+        }
+    };
+}
