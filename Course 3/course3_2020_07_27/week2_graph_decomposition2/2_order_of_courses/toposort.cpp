4036d07676c80f1c815e228a515023077a4b7db1
ducthinh2603 <iscolarcon@gmail.com>
2023-01-30 19:06:54 +0700

    solve week 2

--- Course 3/course3_2020_07_27/week2_graph_decomposition2/2_order_of_courses/toposort.cpp
+++ Course 3/course3_2020_07_27/week2_graph_decomposition2/2_order_of_courses/toposort.cpp
@@ -7,12 +7,44 @@ using std::pair;
 
 void dfs(vector<vector<int> > &adj, vector<int> &used, vector<int> &order, int x) {
   //write your code here
-}     
+} 
+int get_unvisited(bool* visited, int n) {
+  for (int i = 0; i < n; i++) {
+    if (!visited[i]) return i;
+  }
+  return -1;
+}
+
+void explore(int ind, vector<vector<int> >& adj, bool* visited, vector<int> &order) {
+  
+  visited[ind] = true;
+  int m = adj[ind].size();
+  if (m == 0) {
+    order.insert(order.begin(), ind);
+    return;
+  }
+  else {
+    for (int i = 0; i < m; i++) {
+      if (!visited[adj[ind][i]])
+        explore(adj[ind][i], adj, visited, order);
+    }
+    order.insert(order.begin(), ind);
+    return;
+  }
+}    
 
 vector<int> toposort(vector<vector<int> > adj) {
-  vector<int> used(adj.size(), 0);
+  int n = adj.size();
   vector<int> order;
   //write your code here
+  bool* visited = new bool[n];
+  for (int i = 0; i < n; i++) {
+    visited[i] = false;
+  }
+  for (int i = 0; i < n; i++) {
+    if (!visited[i])
+      explore(i, adj, visited, order);    
+  }
   return order;
 }
 
--- Course 3/course3_2020_07_27/week2_graph_decomposition2/3_intersection_reachability/strongly_connected.cpp
+++ Course 3/course3_2020_07_27/week2_graph_decomposition2/3_intersection_reachability/strongly_connected.cpp
@@ -5,9 +5,97 @@
 using std::vector;
 using std::pair;
 
+void explore(int ind, vector<vector<int> >& adj, bool* removed, bool* visited) {
+  if (visited[ind] || removed[ind]) return;
+  visited[ind] = true;
+  int m = adj[ind].size();
+  if (m == 0) {
+    removed[ind] = true;
+    return;
+  }
+  else {
+    bool should_removed = true;
+    for (int i = 0; i < m; i++) {
+      explore(adj[ind][i], adj, removed, visited);
+      should_removed &= removed[adj[ind][i]];
+    }
+    if (should_removed) removed[ind] = true;
+    return;
+  }
+}
+
+void explore(int ind, vector<vector<int> >& adj, bool* visited, vector<int> &order) {
+  
+  visited[ind] = true;
+  int m = adj[ind].size();
+  if (m == 0) {
+    order.push_back(ind);
+    return;
+  }
+  else {
+    for (int i = 0; i < m; i++) {
+      if (!visited[adj[ind][i]])
+        explore(adj[ind][i], adj, visited, order);
+    }
+    order.push_back(ind);
+    return;
+  }
+}    
+
+vector<int> toposort(vector<vector<int> > adj) {
+  int n = adj.size();
+  vector<int> order;
+  //write your code here
+  bool* visited = new bool[n];
+  for (int i = 0; i < n; i++) {
+    visited[i] = false;
+  }
+  for (int i = 0; i < n; i++) {
+    if (!visited[i])
+      explore(i, adj, visited, order);    
+  }
+  return order;
+}
+vector<vector<int> > get_reverse_graph(vector<vector<int> > adj) {
+  int n = adj.size();
+  vector<vector<int> > rev(n, vector<int>());
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < adj[i].size(); j++) {
+      rev[adj[i][j]].push_back(i);
+    }
+  }
+  return rev;
+}
+
+void show_order(vector<int> order) {
+  for (int i = 0; i <= order.size() - 1; i++) {
+    std::cout << order[i] + 1 << " ";
+  }
+  std::cout << "\n";
+}
+
 int number_of_strongly_connected_components(vector<vector<int> > adj) {
   int result = 0;
   //write your code here
+  int n = adj.size();
+  vector<vector<int> > rev = get_reverse_graph(adj);
+  vector<int> order = toposort(rev);
+
+  // show_order(order);
+
+  bool* removed = new bool[n];
+  bool* visited = new bool[n];
+  for (int i = 0; i < n; i++) {
+    removed[i] = false;
+    visited[i] = false;
+  }
+
+  for (int i = order.size() - 1; i >=0; i--) {
+    if (!visited[order[i]] && !removed[order[i]]) {
+      explore(order[i], adj, visited, removed);
+      result++;
+    }
+  }
   return result;
 }
 
