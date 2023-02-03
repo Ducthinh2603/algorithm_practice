407945c7e201aec1c9550a17d4e8fcb7775ad512
thinhduong-gft <thinh.ducduong@gft.com>
2023-02-03 13:33:12 +0700

    solve week4 but shortest paths

--- Course 3/course3_2020_07_27/week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.cpp
+++ Course 3/course3_2020_07_27/week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.cpp
@@ -1,19 +1,78 @@
 #include <iostream>
 #include <vector>
 
+#define INF 100000001
+
 using std::vector;
 
-int negative_cycle(vector<vector<int> > &adj, vector<vector<int> > &cost) {
-  //write your code here
+bool is_negative_cycle(int src, long long* dist, vector<vector<int> > &adj, vector<vector<int> > &cost)
+{
+  int n = adj.size();
+  for (int i = 0; i < n; i++)
+  {
+    dist[i] = INF;
+  }
+  dist[src] = 0;
+  for (int i = 0; i < n - 1; i++)
+  {
+    for (int s = 0; s < n; s++)
+    {
+      for (int k = 0; k < adj[s].size(); k++)
+      {
+        int t = adj[s][k];
+        long long c = cost[s][k];
+        if (dist[s] != INF && dist[t] > dist[s] + c)
+        {
+          dist[t] = dist[s] + c;
+        }
+      }
+    }
+  }
+  for (int s = 0; s < n; s++)
+  {
+    for (int k = 0; k < adj[s].size(); k++)
+    {
+      int t = adj[s][k];
+      long long c = cost[s][k];
+      if (dist[s] != INF && dist[t] > dist[s] + c)
+      {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+int negative_cycle(vector<vector<int> > &adj, vector<vector<int> > &cost)
+{
+  // write your code
+  int n = adj.size();
+  long long* dist = new long long[n];
+  bool* visited = new bool[n];
+  for(int i = 0; i < n; i++) {
+    visited[i] = false;
+  }
+  for (int i = 0; i < n; i++) {
+    if (!visited[i]) {
+      if (is_negative_cycle(i, dist, adj, cost)) return true;
+    }
+    for (int i = 0; i < n; i++) {
+      if (dist[i] != INF) {
+        visited[i] = true;
+      }
+    }
+  }
   return 0;
 }
 
-int main() {
+int main()
+{
   int n, m;
   std::cin >> n >> m;
   vector<vector<int> > adj(n, vector<int>());
   vector<vector<int> > cost(n, vector<int>());
-  for (int i = 0; i < m; i++) {
+  for (int i = 0; i < m; i++)
+  {
     int x, y, w;
     std::cin >> x >> y >> w;
     adj[x - 1].push_back(y - 1);
--- Course 3/course3_2020_07_27/week4_paths_in_graphs2/3_exchanging_money/shortest_paths.cpp
+++ Course 3/course3_2020_07_27/week4_paths_in_graphs2/3_exchanging_money/shortest_paths.cpp
@@ -8,9 +8,48 @@ using std::queue;
 using std::pair;
 using std::priority_queue;
 
+void print(vector<long long> distance) {
+  for (int i = 0; i < distance.size(); i++) {
+    std::cout<< distance[i] << " ";
+  }
+  std::cout<< "\n";
+}
+
 void shortest_paths(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, vector<long long> &distance, vector<int> &reachable, vector<int> &shortest) {
   //write your code here
+  long long INF = std::numeric_limits<long long>::max();
+  int n = adj.size();
+  distance[s] = 0;
+  for(int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n; j++) {
+      for (int k = 0; k < adj[j].size(); k++) {
+        int t = adj[j][k];
+        long long c = cost[j][k];
+        if (distance[j] != INF && distance[t] > distance[j] + c) {
+          distance[t] = distance[j] + c;
+        }
+      }
+    }
+  }
+  for (int i = 0; i < n; i++) {
+    if (distance[i] != INF) {
+      reachable[i] = 1;
+    }
+  }
+  for(int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      for (int k = 0; k < adj[j].size(); k++) {
+        int t = adj[j][k];
+        long long c = cost[j][k];
+        if (distance[j] != INF && distance[t] > distance[j] + c) {
+          distance[t] = distance[j] + c;
+          shortest[t] = 0;
+        }
+      }
+    }
+  }
 }
+
 
 int main() {
   int n, m, s;
